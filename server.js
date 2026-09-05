4502b3a9618de4d7a0089cbecdf7277d84f7a2e2
PasanSasmika <pasansasmika198@gmail.com>
2026-09-05 10:28:04 +0530

    add missing routes and route names

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import pool from "./db.js";
 import authRoutes from "./routes/auth.routes.js";
 import projectRoutes from "./routes/project.routes.js";
 import reportRoutes from "./routes/reports.route.js";
+import dashboardRoutes from "./routes/dashboard.routes.js";
+import userRoutes from "./routes/users.routes.js";
 
 
 dotenv.config();
@@ -28,14 +30,12 @@ app.get(`${API_BASE}/health`, async (req, res) => {
 
 
 
+
 app.use(`${API_BASE}/auth`, authRoutes);
 app.use(`${API_BASE}/projects`, projectRoutes);
-app.use(`${API_BASE}/projects`, reportRoutes);
-
-
-
-
-
+app.use(`${API_BASE}/report`, reportRoutes);
+app.use(`${API_BASE}/dashboard`, dashboardRoutes);
+app.use(`${API_BASE}/users`, userRoutes);
 
 
 
